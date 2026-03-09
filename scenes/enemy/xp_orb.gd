32dbb0e0ae81a8690c4a86956af1b3c60046b63d
Emmanuel Arvantis <emmanuel.arvanitis@gmail.com>
2026-03-09 19:09:34 +0200

    let's try again

--- scenes/enemy/xp_orb.gd
+++ scenes/enemy/xp_orb.gd
@@ -12,7 +12,8 @@ func _process(delta: float) -> void:
 	var dist := global_position.distance_to(car.global_position)
 	if dist < GameManager.pickup_radius or _collecting:
 		_collecting = true
-		global_position = global_position.lerp(car.global_position, delta * 8.0)
+		var to_car := car.global_position - global_position
+		global_position += to_car.normalized() * minf(to_car.length(), 60.0 * delta)
 		if global_position.distance_to(car.global_position) < 0.8:
 			GameManager.add_xp(xp_value)
 			queue_free()
